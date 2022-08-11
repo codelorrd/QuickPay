51acf471444a42950b1545219256a76c06226570
https://github.com/codelorrd <ibrahimtajudeen911@gmail.com>
2022-08-11 18:12:58 +0100

    Modified the html and css file and styled the mobile device width

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("openNav").onclick = function() {
+    document.getElementById("nav-links").style.display = "block";
+}
+
+document.getElementById("closeNav").onclick = function() {
+    document.getElementById("nav-links").style.display = "none";
+}
+
